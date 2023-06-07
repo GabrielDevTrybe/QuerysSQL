6d673dd5d635794fb7e48d5fdbf3ab6a221d16c9
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2023-06-07 10:17:55 -0300

    query de indicator

--- Encontrar_Indicadores.sql
+++ Encontrar_Indicadores.sql
@@ -15,3 +15,28 @@
 
 BRASILEIRÃO B TOTAL HISTÓRICO
  SELECT * FROM indicators WHERE Description = "BRASILEIRÃO B TEMP. ANTERIOR" AND ProjId = 87;
+
+
+
+
+
+
+-- Recebi a tarefa de pegar um nome do indicador na dash do fanbox;
+-- Primeiro eu tive que ver qual era o ProjId que estou querendo esse indicador, no caso 
+-- era o galo, entao na tabela projects eu encontrei o ProjId do galo que era 137, dai
+-- procurar qual era o Id desse indicador "IndID", depois eu trouxe todos os indicadores 
+-- baseado nesse indicador que tem o ID 431 e o time que usa esse indicador.
+-- Os campos era a quantidade que estava la, o percentual e o texto igual estava na tela
+
+-- IndID 431   indicador
+-- projId 137  projects
+
+SELECT * FROM projects WHERE ProjectID = 137;
+SELECT * FROM indicators WHERE ProjId = 137 AND Description = "PLANO DE SAÚDE";
+
+
+
+SELECT REPLACE(`text`, 'otherText=', '') AS Texto, value AS Quantidade, perc AS Percentual
+FROM indicator_values
+WHERE IndID = 431 AND ProjId = 137
+ORDER BY value DESC;
